b33c1f6cf08c285b85a83091cba8e6bee9755ec6
DoubleGio <31928779+DoubleGio@users.noreply.github.com>
2017-10-04 17:58:19 +0200

    Update List.java

--- src/main/java/List.java
+++ src/main/java/List.java
@@ -1,5 +1,10 @@
 public class List<E extends Comparable> implements ListInterface<E>{
 
+	Node firstNode,
+		currentNode,
+		lastNode;
+	int size = 0;
+	
     private class Node {
 
         E data;
@@ -17,64 +22,171 @@ public class List<E extends Comparable> implements ListInterface<E>{
         }
 
     }
+    
+    List() {
+    	
+    }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return firstNode == null;
     }
 
     @Override
     public ListInterface<E> init() {
-        return null;
+    	if (firstNode != null) {
+    		firstNode = null;
+    		currentNode = null;
+    		lastNode = null;
+    		size = 0;
+    	}
+        return this;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
-    @Override
+    private void addExisting(E d) {
+    	Node n = new Node(d, currentNode, currentNode.next);
+    	if (currentNode != lastNode) {
+    		currentNode.next.prior = n;
+    	}
+		currentNode.next = n;
+		currentNode = n;
+    }
+    
+	@SuppressWarnings("unchecked")
+	@Override
     public ListInterface<E> insert(E d) {
-        return null;
+		if (this.find(d)) {
+        	addExisting(d);
+    	} 
+		if (this.isEmpty()) {
+    		Node n = new Node(d);
+    		firstNode = n;
+    		currentNode = n;
+    		lastNode = n;
+    	} else {
+	    	if (firstNode.data.compareTo(d) == 1) {
+	    		Node n = new Node(d, null, firstNode);
+	    		currentNode = n;
+	    		firstNode.prior = n;
+	    		firstNode = n;
+	    	} 
+	    	if (lastNode.data.compareTo(d) == -1){		//WAAROM WERKT NIET BIJ 'c'.compareTo('f')???
+	    		Node n = new Node(d, lastNode, null);
+	    		currentNode = n;
+	    		lastNode.next = n;
+	    		lastNode = n;
+	    	} else {
+	    		currentNode = firstNode;
+	    		while (currentNode != null) {
+	    			if (currentNode.data.compareTo(d) == -1 && currentNode.next.data.compareTo(d) == 1) {
+	    				Node n = new Node(d, currentNode, currentNode.next);
+	    				if (currentNode == firstNode) {
+	    					firstNode.next.prior = n;
+	    					firstNode.next = n;
+	    				}
+	    				currentNode.next.prior = n;
+	    				currentNode.next = n;
+	    				currentNode = n;
+	    			} else {
+	    				currentNode = currentNode.next;
+	    			}
+	    		}
+	    	}
+    	}
+    	size++;
+        return this;
     }
 
     @Override
     public E retrieve() {
-        return null;
+    	/*if (this.isEmpty()) {
+    		throw new APException("blabla");
+    	}*/
+        return currentNode.data;
     }
 
     @Override
     public ListInterface<E> remove() {
-        return null;
+    	if (this.size == 1) {
+    		return init();
+    	} 
+    	if (currentNode == lastNode) {
+    		currentNode.prior.next = null;
+    		currentNode = currentNode.prior;
+    		lastNode = currentNode;
+    	} else {
+    		currentNode.prior.next = currentNode.next;
+    		currentNode.next.prior = currentNode.prior;
+    		currentNode = currentNode.next;
+    	}
+    	size--;
+        return this;
     }
 
     @Override
     public boolean find(E d) {
+    	if (this.isEmpty()) {
+    		return false;
+    	}
+    	if (firstNode.data.compareTo(d) == 0) {
+    		return true;
+    	}
+    	currentNode = firstNode;
+    	while (goToNext()) {
+    		if (currentNode.data.compareTo(d) == 0) {
+    			return true;
+    		}
+    	}
         return false;
     }
 
     @Override
     public boolean goToFirst() {
-        return false;
+    	if (this.isEmpty()) {
+    		return false;
+    	} else {
+    		currentNode = firstNode;
+    		return true;
+    	}
     }
 
     @Override
     public boolean goToLast() {
-        return false;
+    	if (this.isEmpty()) {
+    		return false;
+    	} else {
+    		currentNode = lastNode;
+    		return true;
+    	}
     }
 
     @Override
     public boolean goToNext() {
-        return false;
+    	if (this.isEmpty() || currentNode == lastNode) {
+    		return false;
+    	} else {
+    		currentNode = currentNode.next;
+    		return true;
+    	}
     }
 
     @Override
     public boolean goToPrevious() {
-        return false;
+    	if (this.isEmpty() || currentNode == firstNode) {
+    		return false;
+    	} else {
+    		currentNode = currentNode.prior;
+    		return true;
+    	}
     }
 
     @Override
     public ListInterface<E> copy() {
-        return null;
+        return this;
     }
 }
