5a6f6482c6af04a59ec7b432a3904ebf267a38d6
JaroAmsterdam <31955344+JaroAmsterdam@users.noreply.github.com>
2017-10-09 09:47:03 +0200

    Add files via upload

---
+++ src/main/java/Set.java
@@ -0,0 +1,58 @@
+
+public class Set<E extends Comparable> implements SetInterface<E> {
+		
+	    
+	    public Set<E> init(){
+	    	return null;
+	    }
+		
+	    
+	    public void add(E e){
+	    	
+	    }
+
+	    
+	    public void remove(E e){
+	    	
+	    }
+
+	    
+	    public void set(E e, E d){
+	    	
+	    }
+
+	    
+	    public boolean find(E e){
+	    	return false;
+	    }
+
+	    
+	    public int size(){
+	    	return 0;
+	    }
+		
+	    
+	    public boolean isEmpty(){
+	    	return false;
+	    }
+	    
+	   
+	    public Set union(Set s){
+	    	return null;
+	    }
+	    
+	   
+	    public Set intersection(Set s){
+	    	return null;
+	    }
+	    
+	    
+	    public Set complement(Set s){
+	    	return null;
+	    }
+	    
+	   
+	    public Set symmetricDifference(Set s){
+	    	return null;
+	    }
+	}
