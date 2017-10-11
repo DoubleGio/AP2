373d97b32cc69a4b76e3767396a497b0cc3970a4
DoubleGio <31928779+DoubleGio@users.noreply.github.com>
2017-10-11 16:44:00 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,6 +11,7 @@ public class Main {
 			INTERSECTION = '*',
 			COMPLEMENT = '-',
 			SYM_DIFFERENCE = '|';
+	private static final int STANDARD_LENGTH = 100;
 	
 	private char readChar(Scanner in) {
 		return in.next().charAt(0);
@@ -28,38 +29,73 @@ public class Main {
 		return in.hasNext("[a-zA-Z]");
 	}
 	
-	/*private boolean nextCharIs(char c, char compareTo) {
-		return (c == compareTo);
+	private void print(Scanner in) throws APException {
+		readChar(in);
+		if (nextCharIs(in, ' ')) {
+			readChar(in);
+		} else if (nextCharIs(in, '{')) {
+			readChar(in);
+			Set<Integer> s = readSet(in);
+		} else if (nextCharIs(in, '(')) {
+			
+		} else if (nextCharIsLetter(in)) {
+			
+		} else {
+			throw new APException("Wrong print syntax/n");
+		}
 	}
 	
-	private boolean nextCharIsDigit(char c) {
-		return (c >= 0 && c <= 9);
+	private Set<Integer> readSet(Scanner in) throws APException {
+		Set<Integer> result = new Set<Integer>();
+		char[] digits = new char[STANDARD_LENGTH];
+		int index = -1;
+		while (!nextCharIs(in, '}')) {
+			if (nextCharIsDigit(in)) {
+				index++;
+				if (index == digits.length) {
+					char[] tempStack = new char[digits.length * 2];
+		    			System.arraycopy(digits, 0, tempStack, 0, digits.length);
+		    			digits = tempStack;
+				}
+				digits[index] = readChar(in);
+			} else if (nextCharIs(in, ',')) {
+				result.add(Integer.parseInt(new String(digits)));
+			} else if (nextCharIs(in, ' ')){
+				readChar(in);
+				if (nextCharIsDigit(in)) {
+					throw new APException("Space in between digits");
+				}
+			} else {
+				throw new APException("Incorrect character in set");
+			}
+		}
+		return result;
 	}
 	
-	private boolean nextCharIsLetter(char c) {
-		return (c >= 'A' && c <='Z');
-	}*/
 	
-    private void start() {
+    private void start() throws APException {
         // Create a scanner on System.in
         Scanner in = new Scanner(System.in);
-        String line = in.nextLine();
-        
-        Scanner lineScanner = new Scanner(line);
-		in.useDelimiter("");
-		if (nextCharIs(lineScanner, COMMENT)) {
-			
-		} else if (nextCharIs(lineScanner, PRINT_STATEMENT)) {
-			
-		} else if (nextCharIsLetter(lineScanner)) {
-			
-		}
-        // While there is input, read line and parse it.
-    	//HashMap<Identifier, Set<Integer>> hmap = new HashMap<Identifier, Set<Integer>>();
-    	
+        while (in.hasNextLine()) {
+	        String line = in.nextLine();
+	        
+	        Scanner lineScanner = new Scanner(line);
+			in.useDelimiter("");
+			if (nextCharIs(lineScanner, COMMENT)) {
+				
+			} else if (nextCharIs(lineScanner, PRINT_STATEMENT)) {
+				print(lineScanner);
+				
+			} else if (nextCharIsLetter(lineScanner)) {
+				
+			}
+	        // While there is input, read line and parse it.
+	    	//HashMap<Identifier, Set<Integer>> hmap = new HashMap<Identifier, Set<Integer>>();
+        }
+        in.close();
     }
 
-    public static void main(String[] argv) {
+    public static void main(String[] argv) throws APException {
         new Main().start();
     }
 }
