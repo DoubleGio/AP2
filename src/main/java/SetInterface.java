7263f5b59c8d2c8c354df7210ee6aefe0b98638e
JaroAmsterdam <31955344+JaroAmsterdam@users.noreply.github.com>
2017-10-20 17:10:34 +0200

    Update SetInterface.java

--- src/main/java/SetInterface.java
+++ src/main/java/SetInterface.java
@@ -10,26 +10,27 @@
 
 public interface SetInterface<E extends Comparable<E>> {
 	
-	/** @precondition  -
+	/** 
+	 *  @precondition  -
      *	@postcondition - set-POST is empty and has been returned.
      **/
     SetInterface<E> init();
 	
     /**
+     * @param e The element that has to be added to the Set.
      * @precondition  -
      * @postcondition The element E has been added at the end of the Set if it's not already there.
      */
     void add(E e);
 
     /**
-     *
+     * @param e The element that has to be removed from the Set.
      * @precondition  The set is not empty.
      * @postcondition The specified element has been removed. The size of the Set has been reduced by 1.
      */
     void remove(E e);
 
     /**
-     *
      * @param e The element that has to be retrieved.
      * @precondition  The set is not empty.
      * @postcondition A boolean specifying whether the element is present in the list.
@@ -52,26 +53,31 @@ public interface SetInterface<E extends Comparable<E>> {
      * @precondition  -
      * @postcondition A deep copy of the Set has been returned.
      */
+    SetInterface<E> copy();
     
     /**
+     * @param s The set on which the Union operation has to be performed together with the Set in which this method is called.
      * @precondition  -
      * @postcondition The union of 2 sets has been returned, or an error is has been thrown.
      */
     SetInterface<E> union(Set<E> s);
     
     /**
+     * @param s The set on which the Intersection operation has to be performed together with the Set in which this method is called.
      * @precondition  -
      * @postcondition The intersection of 2 sets has been returned, or an error is has been thrown.
      */
     SetInterface<E> intersection(Set<E> s);
     
     /**
+     * @param s The set on which the Complement operation has to be performed together with the Set in which this method is called.
      * @precondition  -
      * @postcondition The complement of 2 sets has been returned, or an error is has been thrown.
      */
     SetInterface<E> complement(Set<E> s);
     
     /**
+     * @param s The set on which the Symmetric Difference operation has to be performed together with the set in which this method is called.
      * @precondition  -
      * @postcondition The symmetric difference of 2 sets has been returned, or an error is has been thrown.
      */
