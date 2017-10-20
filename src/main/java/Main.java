592bf472d194f979795546da34730191f9dceb26
DoubleGio <31928779+DoubleGio@users.noreply.github.com>
2017-10-20 17:02:18 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -128,88 +128,101 @@ public class Main {
 		}
 		while (!nextCharIs(in, '}')) {
 			skipSpaces(in);
-			String number = "";
 			if (!nextCharIsDigit(in)) {
 				throw new APException("Incorrect character in set");
 			}
-			while (nextCharIsDigit(in)) {
-				if (number.isEmpty() && nextCharIs(in, '0')) {
-					char c = readChar(in);
-					if (nextCharIsDigit(in)) {
-						throw new APException("Character other than '}' or ',' after a 0 not allowed");
-					} else {
-						number += c;
-					}
-				} else {
-					number += readChar(in);
-				}
-			}
+			String number = readNumber(in);
 			skipSpaces(in);
-			if (nextCharIsDigit(in)) {
-				throw new APException("Space in between digits");
-			} else if (nextCharIs(in, ',')) {
-				readChar(in);
-				skipSpaces(in);
-				if (!nextCharIsDigit(in)) {
-					throw new APException("Digit expected after ','");
-				}
-				result.add(new BigInteger(number));
-			} else if (nextCharIs(in, '}')) {
-				result.add(new BigInteger(number));
-			} else {
-				throw new APException("Incorrect character in set");
-			}
+			addNumber(in, result, number);
 		}
 		readChar(in);
 		return result;
 	}
+	
+	private String readNumber(Scanner in) throws APException {
+		String number = "";
+		while (nextCharIsDigit(in)) {
+			if (number.isEmpty() && nextCharIs(in, '0')) {
+				char c = readChar(in);
+				if (nextCharIsDigit(in)) {
+					throw new APException("Character other than '}' or ',' after a 0 not allowed");
+				} else {
+					number += c;
+				}
+			} else {
+				number += readChar(in);
+			}
+		}
+		return number;
+	}
+	
+	private void addNumber(Scanner in, Set<BigInteger> result, String number) throws APException {
+		if (nextCharIsDigit(in)) {
+			throw new APException("Space in between digits");
+		} else if (nextCharIs(in, ',')) {
+			readChar(in);
+			skipSpaces(in);
+			if (!nextCharIsDigit(in)) {
+				throw new APException("Digit expected after ','");
+			}
+			result.add(new BigInteger(number));
+		} else if (nextCharIs(in, '}')) {
+			result.add(new BigInteger(number));
+		} else {
+			throw new APException("Incorrect character in set");
+		}
+	}
+	
+	private void printStatement(Scanner in) throws APException {
+		readChar(in);
+		Set<BigInteger> s = checkExpression(in);
+		skipSpaces(in);
+		if (in.hasNext()) {
+			throw new APException("Incorrect Syntax");
+		}
+		printSet(s);
+	}
+	
+	private void assignment(Scanner in) throws APException {
+		Identifier id = readIdentifier(in);
+		skipSpaces(in);
+		if (!nextCharIs(in, ASSIGNMENT)) {
+			throw new APException("'=' expected in assignment");
+		} else {
+			readChar(in);
+			skipSpaces(in);
+			Set<BigInteger> s = checkExpression(in);
+			skipSpaces(in);
+			if (in.hasNext()) {
+				throw new APException("Incorrect Syntax");
+			}
+			hmap.put(id, s);
+		}
+	}
 		
 	private void start() throws APException {
-        // Create a scanner on System.in
         Scanner in = new Scanner(System.in);
         hmap = new HashMap<Identifier, Set<BigInteger>>();
         
-        // While there is input, read line and parse it.
         while (in.hasNextLine()) {
         	try {
 		        String line = in.nextLine(); 
 		        Scanner lineScanner = new Scanner(line);
 		        
-				
 		        lineScanner.useDelimiter("");
 				skipSpaces(lineScanner);
 				if (nextCharIs(lineScanner, COMMENT)) {
-					//do nothing
+					;	//do nothing
 				} else if (nextCharIs(lineScanner, PRINT_STATEMENT)) {
-					readChar(lineScanner);
-					Set<BigInteger> s = checkExpression(lineScanner);
-					skipSpaces(lineScanner);
-					if (lineScanner.hasNext()) {
-						throw new APException("Incorrect Syntax");
-					}
-					printSet(s);
+					printStatement(lineScanner);
 				} else if (nextCharIsLetter(lineScanner)) {
-					Identifier id = readIdentifier(lineScanner);
-					skipSpaces(lineScanner);
-					if (!nextCharIs(lineScanner, ASSIGNMENT)) {
-						throw new APException("'=' expected in assignment");
-					} else {
-						readChar(lineScanner);
-						skipSpaces(lineScanner);
-						Set<BigInteger> s = checkExpression(lineScanner);
-						skipSpaces(lineScanner);
-						if (lineScanner.hasNext()) {
-							throw new APException("Incorrect Syntax");
-						}
-						hmap.put(id, s);
-					}
+					assignment(lineScanner);
 				} else {
 					throw new APException("Empty line");
 				}
         	} catch (APException e) {
         		out.println(e);
         	}
-        
         }
         in.close();
     }
